03ea467f73862abfcace2a1ef4f6e803806658db
Byaidu <909756245@qq.com>
2021-02-05 03:04:35 +0800

    v2.1

--- dmzj-helper.js
+++ dmzj-helper.js
@@ -1,16 +1,19 @@
 // ==UserScript==
 // @name         ☄️动漫之家增强☄️
 // @namespace    http://tampermonkey.net/
-// @version      1.9
-// @description  动漫之家去广告🚫，对日漫版漫画页进行增强：并排布局📖、图片高度自适应↕️、辅助翻页↔️、页码显示⏱、暗夜模式🌙
+// @version      2.1
+// @description  动漫之家去广告🚫，对日漫版漫画页进行增强：并排布局📖、图片高度自适应↕️、辅助翻页↔️、页码显示⏱、暗夜模式🌙，请设置即时注入模式以避免页面闪烁⚠️
 // @author       Byaidu
 // @match        *.dmzj.com/*
 // @resource     animate_css https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css
+// @resource     element_css https://unpkg.com/element-ui@2.15.0/lib/theme-chalk/index.css
 // @require      https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.min.js
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js
 // @require      https://cdn.jsdelivr.net/npm/jquery.cookie@1.4.1/jquery.cookie.js
+// @require      https://unpkg.com/element-ui@2.15.0/lib/index.js
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
+// @grant        GM_xmlhttpRequest
 // @run-at       document-start
 // ==/UserScript==
 
@@ -33,6 +36,14 @@
             $.cookie('display_mode',1,{expires:999999,path:'/'});
             location.reload();
         }
+        //修改滚动条样式
+        GM_addStyle('::-webkit-scrollbar {width: 4px;}')
+        GM_addStyle('::-webkit-scrollbar-thumb {background-color: rgb(48,48,48);border-radius: 2px;}')
+        //修改element-ui样式
+        GM_addStyle('.el-menu{border-right:0px !important;}')
+        GM_addStyle('.el-drawer__wrapper{width:20%;}')
+        GM_addStyle('.el-drawer{background:transparent !important;}')
+        GM_addStyle('.el-drawer__body{background:rgba(0,0,0,.8) !important;overflow-y: auto}')
         //漫画并页排布
         GM_addStyle('#center_box{justify-content:center;flex-direction: row-reverse;display: flex;flex-wrap: wrap;}')
         //漫画高度自适应
@@ -44,123 +55,220 @@
         GM_addStyle('.hotrm_about{color:#4d4d4d !important;}')
         GM_addStyle('*[style*="display:inline;float:left"]{color:#4d4d4d !important;}')
         GM_addStyle('*[style*="display:inline;float:right"]{display:none !important;}')
-        //引入animate.css
+        //引入css
         const animate_css = GM_getResourceText("animate_css");
+        const element_css = GM_getResourceText("element_css");
         GM_addStyle(animate_css);
+        GM_addStyle(element_css);
         GM_addStyle(':root{--animate-duration:500ms;}')
-        //添加右下角菜单
-        let info = "<div id=\"info\" @mouseover=\"show=1\" @mouseleave=\"show=0\">"+
-            "<transition name=\"custom-classes-transition\" enter-active-class=\"animate__animated animate__fadeIn\" leave-active-class=\"animate__animated animate__fadeOut\">"+
-            "<template v-if=\"show\"><div id=\"info_skip\" class=\"info_item\" @click=\"switch_skip\" style=\"cursor:pointer;\">📖更改跨页</div></template></transition>"+
-            "<transition name=\"custom-classes-transition\" enter-active-class=\"animate__animated animate__fadeIn\" leave-active-class=\"animate__animated animate__fadeOut\">"+
-            "<template v-if=\"show\"><div id=\"info_switch\" class=\"info_item\" @click=\"switch_night\" style=\"cursor:pointer;\">{{message_switch}}</div></template></transition>"+
-            "<template><div id=\"info_count\" class=\"info_item\">{{message_count}}</div></template>"+
-            "</div>";
-        let $info = $(info);
-        $("body").append($info);
-        let info_style = "#info {"+
-            "bottom: 2%;"+
-            "right: 2%;"+
-            "padding: 5px 5px;"+
-            "background: rgba(48,48,48,.7) !important;"+
-            "position: fixed;"+
-            "color: rgba(255,255,255,.7);"+
-            "border-radius: 3px;"+
-            "}";
-        GM_addStyle(info_style)
-        GM_addStyle(".info_item{padding:5px 0px;width:120px;}")
+        //隐藏顶栏
+        GM_addStyle(".hide_head .header-box,.hide_head .funcdiv{display:none !important;}")
+        //更改跨页
         GM_addStyle('.skip{display:none !important;}')
-        //漫画页暗夜模式
-        //日夜模式需要切换的元素
-        GM_addStyle(".theme_dark .mainNav,.theme_dark .header-box,.theme_dark .display_graybg,.theme_dark body,.theme_dark .funcdiv{background:#212121 !important;}")
+        //读取cookie
+        GM_addStyle(".dark_mode .mainNav,.dark_mode .header-box,.dark_mode .display_graybg,.dark_mode body{background:#212121 !important;}")
         if ($.cookie('dark_mode') === undefined) { $.cookie('dark_mode',true,{expires:999999,path:'/'}); }
+        if ($.cookie('hide_head') === undefined) { $.cookie('hide_head',false,{expires:999999,path:'/'}); }
         var dark_mode = $.cookie('dark_mode')=='true';
+        var hide_head = $.cookie('hide_head')=='true';
+        //暗夜模式
         if (dark_mode){
-            $('html').addClass('theme_dark');
+            $('html').addClass('dark_mode');
         }else{
-            $('html').removeClass('theme_dark');
+            $('html').removeClass('dark_mode');
         }
-        //vue绑定右下角菜单
-        var info_app = new Vue({
-            el: '#info',
-            data: {
-                dark:dark_mode,
-                show:0,
-                img_id:0,
-                skip:0
-            },
-            computed: {
-                message_switch:  function () {
-                    return this.dark?'☀️日间模式':'🌙夜间模式'
-                },
-                message_count:  function () {
-                    return this.img_id+'/'+g_max_pic_count
+        //隐藏顶栏
+        if (hide_head){
+            $('html').addClass('hide_head');
+        }else{
+            $('html').removeClass('hide_head');
+        }
+        //延迟加载
+        $(function(){
+            //上下方向键滚动页面，左右方向键切换章节
+            let img_id=0;
+            $("body").keydown(function(event) {
+                if (event.keyCode == 38) {
+                    if (img_id>=1){
+                        if ($("#img_"+img_id).length>0&&$("#img_"+(img_id-1)).length>0&&$("#img_"+img_id).offset().top==$("#img_"+(img_id-1)).offset().top){
+                            img_id-=2;
+                        }else{
+                            img_id-=1;
+                        }
+                    }
+                    info_app.img_id=img_id;
+                    if (img_id!=0) $("html,body").stop()
+                    $("html,body").animate({scrollTop: $("#img_"+img_id).offset().top}, 500);
+                } else if (event.keyCode == 40) {
+                    if (img_id<=g_max_pic_count){
+                        if ($("#img_"+img_id).length>0&&$("#img_"+(img_id+1)).length>0&&$("#img_"+img_id).offset().top==$("#img_"+(img_id+1)).offset().top){
+                            img_id+=2;
+                        }else{
+                            img_id+=1;
+                        }
+                    }
+                    info_app.img_id=img_id;
+                    if (img_id!=g_max_pic_count+1) $("html,body").stop()
+                    $("html,body").animate({scrollTop: $("#img_"+img_id).offset().top}, 500);
+                } else if (event.keyCode == 37) {
+                    let location_new = $("#prev_chapter").attr("href");
+                    if(location_new.indexOf("shtml")>=0)
+                        location.href = location_new;
+                } else if (event.keyCode == 39) {
+                    let location_new = $("#next_chapter").attr("href");
+                    if(location_new.indexOf("shtml")>=0)
+                        location.href = location_new;
                 }
-            },
-            methods:{
-                switch_night: function(){
-                    this.dark=!this.dark
-                    $.cookie('dark_mode',this.dark,{expires:999999,path:'/'});
-                    if (this.dark){
-                        $('html').addClass('theme_dark');
-                    }else{
-                        $('html').removeClass('theme_dark');
+            })
+            //resize事件触发图片和浏览器对齐
+            $(window).resize(function() {
+                $("html,body").animate({scrollTop: $("#img_"+img_id).offset().top}, 0);
+            })
+            //去除原来的keydown事件
+            //https://stackoverflow.com/questions/5436874/how-do-i-unbind-jquery-event-handlers-in-greasemonkey
+            window.addEventListener('load', function (){
+                jQuery = unsafeWindow['jQuery'];
+                jQuery("body").off("keydown");
+            })
+            //添加右下角菜单
+            let info = `
+<div id="info" @mouseover="show=1" @mouseleave="show=0">
+<transition name="custom-classes-transition" enter-active-class="animate__animated animate__fadeIn" leave-active-class="animate__animated animate__fadeOut">
+<template v-if="show"><div id="info_head" class="info_item" @click="switch_head" style="cursor:pointer;">{{message_head}}</div></template></transition>
+<transition name="custom-classes-transition" enter-active-class="animate__animated animate__fadeIn" leave-active-class="animate__animated animate__fadeOut">
+<template v-if="show"><div id="info_skip" class="info_item" @click="switch_skip" style="cursor:pointer;">{{message_skip}}</div></template></transition>
+<transition name="custom-classes-transition" enter-active-class="animate__animated animate__fadeIn" leave-active-class="animate__animated animate__fadeOut">
+<template v-if="show"><div id="info_switch" class="info_item" @click="switch_night" style="cursor:pointer;">{{message_switch}}</div></template></transition>
+<template><div id="info_count" class="info_item">{{message_count}}</div></template>
+</div>`;
+            let $info = $(info);
+            $("body").append($info);
+            let info_style = `
+#info {
+bottom: 2%;
+right: 2%;
+padding: 5px 5px;
+background: rgba(48,48,48,.7) !important;
+position: fixed;
+color: rgba(255,255,255,.7);
+border-radius: 3px;
+}
+.info_item{
+padding:5px 0px;
+width:120px;
+}`;
+            GM_addStyle(info_style);
+            //vue绑定右下角菜单
+            var info_app = new Vue({
+                el: '#info',
+                data: {
+                    dark:dark_mode,
+                    show:0,
+                    img_id:0,
+                    skip:0,
+                    head:hide_head
+                },
+                computed: {
+                    message_switch:  function () {
+                        return this.dark?'☀️日间模式':'🌙夜间模式'
+                    },
+                    message_head:  function () {
+                        return this.head?'📂显示顶栏':'📁隐藏顶栏'
+                    },
+                    message_skip:  function () {
+                        return '📖更改跨页'
+                    },
+                    message_count:  function () {
+                        return this.img_id+'/'+g_max_pic_count
                     }
                 },
-                switch_skip: function(){
-                    this.skip=!this.skip
-                    if (this.skip){
-                        $("#center_box>div:first-child").addClass('skip');
-                    }else{
-                        $("#center_box>div:first-child").removeClass('skip');
-                    }
+                methods:{
+                    switch_night: function(){
+                        this.dark=!this.dark
+                        $.cookie('dark_mode',this.dark,{expires:999999,path:'/'});
+                        if (this.dark){
+                            $('html').addClass('dark_mode');
+                        }else{
+                            $('html').removeClass('dark_mode');
+                        }
+                    },
+                    switch_skip: function(){
+                        this.skip=!this.skip
+                        if (this.skip){
+                            $("#center_box>div:first-child").addClass('skip');
+                        }else{
+                            $("#center_box>div:first-child").removeClass('skip');
+                        }
+                    },
+                    switch_head: function(){
+                        this.head=!this.head
+                        $.cookie('hide_head',this.head,{expires:999999,path:'/'});
+                        if (this.head){
+                            $('html').addClass('hide_head');
+                        }else{
+                            $('html').removeClass('hide_head');
+                        }
+                    },
+                }
+            })
+            //加载目录
+            GM_xmlhttpRequest({
+                method: "GET",
+                headers: {"User-Agent": navigator.userAgent},
+                url: $('.btn2').attr('href'),
+                onload: (res) => {
+                    let response = res.response
+                    var el = $( '<div></div>' );
+                    el.html(response);
+                    let $border=$('.cartoon_online_border a', el);
+                    $.each($border,function(){
+                        console.log(this.title+this.href)
+                        sidebar_app.items.push({
+                            title:this.title,
+                            href:this.href,
+                        })
+                    })
+                }
+            })
+            //添加侧边目录栏
+            let sidebar=`
+<div id="sidebar" @mouseleave="drawer=false">
+<div id="toggle" @mouseover="drawer=true" style="top:0px;left:0px;height:100vh;width:10vw;position: fixed;"></div>
+<el-drawer
+title="我是标题"
+:size="size"
+:modal="modal"
+:visible="drawer"
+:with-header="false"
+:direction="direction">
+<el-menu background-color="transparent"
+text-color="#fff"
+active-text-color="#ffd04b"
+@select="handleSelect">
+<template v-for="(item, index) in items">
+<el-menu-item v-bind:index="index">{{item.title}}</el-menu-item>
+</template>
+</el-menu>
+</el-drawer>
+</div>`
+            let $sidebar = $(sidebar);
+            $("body").append($sidebar);
+            //vue绑定侧边目录栏
+            var sidebar_app = new Vue({
+                el: '#sidebar',
+                data: {
+                    drawer: false,
+                    size:'100%',
+                    modal:false,
+                    direction: 'ltr',
+                    items: [],
                 },
-            }
-        })
-        //上下方向键滚动页面，左右方向键切换章节
-        let img_id=0;
-        $("body").keydown(function(event) {
-            if (event.keyCode == 38) {
-                if (img_id>=1){
-                    if ($("#img_"+img_id).length>0&&$("#img_"+(img_id-1)).length>0&&$("#img_"+img_id).offset().top==$("#img_"+(img_id-1)).offset().top){
-                        img_id-=2;
-                    }else{
-                        img_id-=1;
+                methods:{
+                    handleSelect(key) {
+                        location.href=this.items[key].href;
                     }
                 }
-                info_app.img_id=img_id;
-                if (img_id!=0) $("html,body").stop()
-                $("html,body").animate({scrollTop: $("#img_"+img_id).offset().top}, 500);
-            } else if (event.keyCode == 40) {
-                if (img_id<=g_max_pic_count){
-                    if ($("#img_"+img_id).length>0&&$("#img_"+(img_id+1)).length>0&&$("#img_"+img_id).offset().top==$("#img_"+(img_id+1)).offset().top){
-                        img_id+=2;
-                    }else{
-                        img_id+=1;
-                    }
-                }
-                info_app.img_id=img_id;
-                if (img_id!=g_max_pic_count+1) $("html,body").stop()
-                $("html,body").animate({scrollTop: $("#img_"+img_id).offset().top}, 500);
-            } else if (event.keyCode == 37) {
-                let location_new = $("#prev_chapter").attr("href");
-                if(location_new.indexOf("shtml")>=0)
-                    location.href = location_new;
-            } else if (event.keyCode == 39) {
-                let location_new = $("#next_chapter").attr("href");
-                if(location_new.indexOf("shtml")>=0)
-                    location.href = location_new;
-            }
-        })
-        //resize事件触发图片和浏览器对齐
-        $(window).resize(function() {
-            $("html,body").animate({scrollTop: $("#img_"+img_id).offset().top}, 0);
-        })
-        //去除原来的keydown事件
-        //https://stackoverflow.com/questions/5436874/how-do-i-unbind-jquery-event-handlers-in-greasemonkey
-        window.addEventListener('load', function (){
-            jQuery = unsafeWindow['jQuery'];
-            jQuery("body").off("keydown");
-        })
+            })
+            })
     }
 })();
